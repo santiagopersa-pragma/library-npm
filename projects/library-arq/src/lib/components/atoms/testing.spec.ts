579dbff0f6c6d4f5233feff4cb48118c0d8da1ed
Santiago Pérez <santiago.perez@pragma.com.co>
2025-08-01 11:29:15 -0500

    chore: add jest config

--- projects/library-arq/src/lib/components/atoms/testing.spec.ts
+++
@@ -1,7 +0,0 @@
-import { testing } from './testing';
-
-describe('testing', () => {
-  it('should works', () => {
-    expect(testing()).toBe('hello world');
-  });
-});
--- projects/library-arq/src/lib/components/atoms/testing.ts
+++
@@ -1,3 +0,0 @@
-export function testing() {
-  return 'hello world';
-}
